b0aa958fbbdfa49fcd5dc8ae2b405b0986a9eb6a
Mohamed Keridou <johndoe@example.com>
2021-11-10 10:56:33 +0100

    done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import contacts from './contacts.json'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const fiveContact = contacts.slice(0,5)
+
+
+export class App extends Component {
+  state = {
+    contacts : fiveContact
+  }
+
+ addRandom = () => {
+   const stateCopy = [ ...this.state.contacts]
+   stateCopy.push(contacts[Math.floor(Math.random() * contacts.length)])
+
+   this.setState({
+     contacts: stateCopy
+   })
+ }
+
+ sortByName = () => {
+   const stateCopy = [ ...this.state.contacts]
+   stateCopy.sort((a,b) => a.name.localeCompare(b.name))
+
+   this.setState({
+     contacts: stateCopy
+   })
+ }
+
+
+
+  render() {
+    return (
+      <div className='App'>
+        <button onClick={this.addRandom} >Add a random contact</button>
+        <button onClick={this.sortByName} >Sort contacts by name</button>
+        <table>
+          <thead>
+            <tr>
+              <td>Picture</td>
+              <td>Name</td>
+              <td>Popularity</td>
+            </tr>
+          </thead>
+
+          <tbody>
+            {this.state.contacts.map(celebrity => {
+              return (
+                <tr>
+                 
+                 <td><img src={celebrity.pictureUrl}></img></td>
+                  <td>{celebrity.name}</td>
+                  <td>{celebrity.popularity}</td>
+                  
+               </tr>
+              )
+            }) }
+          </tbody>
+        </table>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
